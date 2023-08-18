55c67dfe6fd7f090b5e36f5c9420d256edf4ff99
haky <57565164+sulebread@users.noreply.github.com>
2023-08-19 02:39:29 +0900

    footer 추가

--- nav.js
+++ nav.js
@@ -28,7 +28,19 @@ const headerNavContent = `
     </div>
 `;
 
+const footerNavContent = `
+    <div class="footerNav">
+        <nav>
+            <a href="https://www.instagram.com/ktwiz.pr" target="_blank"> <img src="./media/insta_logo.png" style="width:50px; height:50px;"> </a>
+            <a href="https://www.youtube.com/@ktwiztv" target="_blank"> <img src="./media/youtube_logo.png" style="width:50px; height:50px;"> </a>
+        </nav>
+        <font color="white"> <h3> 제작자 : 방한경 </h3> </font>
+    </div>
+`;
+
 window.onload = function(){
     const headerNav = document.getElementById("headerNav");
     headerNav.innerHTML = headerNavContent;
+    const footerNav = document.getElementById("footerNav");
+    footerNav.innerHTML = footerNavContent;
 }

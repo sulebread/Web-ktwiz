7ca8b05ba36f3a972857bfeaae8d580040e70c75
haky <57565164+sulebread@users.noreply.github.com>
2023-08-19 04:00:45 +0900

    사진 추가

--- mediaPhotoList.js
+++ mediaPhotoList.js
@@ -23,6 +23,54 @@ const photoList = {
             "Title":"배정대선수!",
             "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/18/20230818102637.296-15d453c54c3d.jpg",
             "date": "2023.08.18"
+        },
+        {
+            "subTitle": "8월 16일 두산과의 경기",
+            "Title":"선수들!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/17/20230817101613.207-57724c7c7aae.jpg",
+            "date": "2023.08.17"
+        },
+        {
+            "subTitle": "8월 16일 두산과의 경기",
+            "Title":"엄상백선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/17/20230817101554.e2f-ba459ed9d6bd.jpg",
+            "date": "2023.08.17"
+        },
+        {
+            "subTitle": "8월 15일 두산과의 경기",
+            "Title":"쿠에바스선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/16/20230816104254.a1c-8045122e6d2b.jpg",
+            "date": "2023.08.16"
+        },
+        {
+            "subTitle": "8월 15일 두산과의 경기",
+            "Title":"김민혁선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/16/20230816104253.fbb-9c032dd5d524.jpg",
+            "date": "2023.08.16"
+        },
+        {
+            "subTitle": "8월 2일 SSG와의 경기",
+            "Title":"황재균선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/03/20230803131655.8ba-9ec54bb85507.jpg",
+            "date": "2023.08.03"
+        },
+        {
+            "subTitle": "8월 2일 SSG와의 경기",
+            "Title":"문상철선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/03/20230803131655.b34-a1e39014999a.jpg",
+            "date": "2023.08.03"
+        },
+        {
+            "subTitle": "7월 28일 NC와의 경기",
+            "Title":"알포드선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/08/01/20230801130949.1fd-9522b99a7e9b.jpg",
+            "date": "2023.08.01"
+        },
+        {
+            "subTitle": "7월 15일 올스타전",
+            "Title":"김상수선수!",
+            "url": "https://wizzap.ktwiz.co.kr/files/article/2023/07/17/20230717101910.b1c-7729def15e21.jpg",
+            "date": "2023.07.17"
         }
     ]
 }
